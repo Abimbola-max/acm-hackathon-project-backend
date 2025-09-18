c8b683a840ccc0b08837f0387e07f19e191a3a6f
Abisoye Abimbola <haryishar2@gmail.com>
2025-09-18 15:32:56 +0100

    untrack dey stress me

--- intelligence/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import PlatformInsight
-
-@admin.register(PlatformInsight)
-class PlatformInsightAdmin(admin.ModelAdmin):
-    list_display = ('artist', 'platform', 'track_name', 'insight_type', 'value', 'timestamp')
-    list_filter = ('platform', 'insight_type', 'timestamp')
-    search_fields = ('artist__username', 'track_name')
